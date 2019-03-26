de46690afe8de7314eeef6e58ed73764e84de550
root <root@p049i05cert01.unix.local>
2019-03-26 17:18:41 +0300

    edit settings

--- docker-moloch/scripts/buildmoloch.sh
+++ docker-moloch/scripts/buildmoloch.sh
@@ -12,7 +12,7 @@ echo "git clone"
 git clone --recursive https://github.com/aol/moloch.git $BUILDDIR
 echo "cd to dir and build"
 cd $BUILDDIR
-USEPFRING=no ESMEM="512M" DONOTSTART=yes MOLOCHUSER=daemon GROUPNAME=daemon PASSWORD=${PASSWORD_MOLOCH_SHELL} INTERFACE=eth0 BATCHRUN=yes ./easybutton-build.sh
+USEPFRING=no ESMEM="4G" DONOTSTART=yes MOLOCHUSER=daemon GROUPNAME=daemon PASSWORD=${PASSWORD_MOLOCH_SHELL} INTERFACE=eth0 BATCHRUN=yes ./easybutton-build.sh
 make
 make install
 cd /data/moloch/viewer
